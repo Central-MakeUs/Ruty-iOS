7b25fbb3ffc48fc7d142428075f79bd16bdeb9b9
SeongHuiJung <tjdgml150237@naver.com>
2025-01-08 18:09:44 +0900

    [Add] 프로젝트 세팅

---
+++ Ruty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ruty
+//
+//  Created by 정성희 on 1/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
