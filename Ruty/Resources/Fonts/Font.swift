29e08e4e3d5bb89b9598e8d8bae82fd2423cf13b
SeongHuiJung <tjdgml150237@naver.com>
2025-01-11 00:56:04 +0900

    [Add] 폰트추가, 회원가입창 프로그래스바 구현

---
+++ Ruty/Resources/Fonts/Font.swift
@@ -0,0 +1,20 @@
+//
+//  Font.swift
+//  Ruty
+//
+//  Created by 정성희 on 1/11/25.
+//
+
+import Foundation
+
+enum Font : String {
+    case regular = "Pretendard-Regular"
+    case bold = "Pretendard-Bold"
+    case Light = "Pretendard-Light"
+    case Medium = "Pretendard-Medium"
+    case SemiBold = "Pretendard-SemiBold"
+    case Thin = "Pretendard-Thin"
+    case ExtraLight = "Pretendard-ExtraLight"
+    case ExtraBold = "Pretendard-ExtraBold"
+    case Black = "Pretendard-Black"
+}
--- Ruty/ViewController/Login/SignUpAgreeViewController.swift
+++ Ruty/ViewController/Login/SignUpAgreeViewController.swift
@@ -6,24 +6,48 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class SignUpAgreeViewController: UIViewController {
 
+    private let progressBarView = ProgressBarView().then {
+        $0.layer.cornerRadius = 4
+    }
+
+    let titleLabel = UILabel().then {
+        $0.text = """
+이용약관에
+동의해 주세요
+"""
+        $0.textColor = UIColor(3, 7, 18, 1)
+        $0.textAlignment = .left
+        $0.font = UIFont(name: Font.SemiBold.rawValue, size: 24)
+        $0.numberOfLines = 0
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
+        
+        setLayout()
+        self.progressBarView.ratio = 0.0
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setLayout() {
+        [progressBarView, titleLabel].forEach({ view.addSubview($0) })
+        
+        self.progressBarView.snp.makeConstraints {
+            $0.left.right.equalTo(self.view.safeAreaLayoutGuide).inset(20)
+            $0.top.equalTo(self.view.safeAreaLayoutGuide)
+            $0.height.equalTo(8)
+        }
+        
+        self.titleLabel.snp.makeConstraints({
+            $0.top.equalTo(progressBarView).offset(48)
+            $0.leading.trailing.equalToSuperview().inset(20)
+        })
     }
-    */
 
 }
