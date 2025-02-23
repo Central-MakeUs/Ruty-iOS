2a8033a147d22416add5a15a0774203641d61451
SeongHuiJung <tjdgml150237@naver.com>
2025-02-23 15:36:49 +0900

    [Design] 로그인 UI 변경

--- Ruty/ViewController/Login/LoginViewController.swift
+++ Ruty/ViewController/Login/LoginViewController.swift
@@ -14,27 +14,84 @@ import AuthenticationServices
 
 class LoginViewController: UIViewController {
     
-    let googleLoginBtn = UIButton().then {
-        $0.setTitle("Google 로그인", for: .normal)
-        $0.backgroundColor = .black
-        $0.layer.cornerRadius = 10
-        $0.setTitleColor(.white, for: .normal)
-        $0.addTarget(self, action: #selector(tapGoogleLoginBtn(_:)), for: .touchUpInside)
+    private let RutyIcon = UIImageView().then {
+        $0.backgroundColor = .clear
+        $0.image = UIImage(named: "Ruty-img")
     }
     
-    private let appleLoginBtn = ASAuthorizationAppleIDButton(authorizationButtonType: .continue, authorizationButtonStyle: .black).then {
-        $0.cornerRadius = 10
-        $0.addTarget(self, action: #selector(tapAppleLoginBtn), for: .touchUpInside)
+    private let RutyLogo = UIImageView().then {
+        $0.backgroundColor = .clear
+        $0.image = UIImage(named: "Ruty-title")
     }
     
+    private let googleLoginView = UIView().then {
+        $0.backgroundColor = UIColor.fill.secondary
+        $0.layer.cornerRadius = 16
+        $0.isUserInteractionEnabled = true
+    }
+    
+    let googleStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 12
+        $0.alignment = .center
+        $0.distribution = .equalCentering
+    }
+    
+    private let googleLoginLabel = UILabel().then {
+        $0.text = "Google로 로그인"
+        $0.textColor = UIColor.font.primary
+        $0.textAlignment = .left
+        $0.font = UIFont(name: Font.semiBold.rawValue, size: 16)
+        $0.numberOfLines = 1
+    }
+    
+    private let googleLoginLabelLogo = UIImageView().then {
+        $0.backgroundColor = .clear
+        $0.image = UIImage(named: "Logo-Google")
+    }
+    
+    private let appleLoginView = UIView().then {
+        $0.backgroundColor = UIColor.fill.secondary
+        $0.layer.cornerRadius = 16
+        $0.isUserInteractionEnabled = true
+    }
+    
+    let appleStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.spacing = 12
+        $0.alignment = .center
+        $0.distribution = .equalCentering
+    }
+    
+    private let appleLoginLabel = UILabel().then {
+        $0.text = "Apple로 로그인"
+        $0.textColor = UIColor.font.primary
+        $0.textAlignment = .left
+        $0.font = UIFont(name: Font.semiBold.rawValue, size: 16)
+        $0.numberOfLines = 1
+    }
+    
+    private let appleLoginLabelLogo = UIImageView().then {
+        $0.backgroundColor = .clear
+        $0.image = UIImage(named: "Logo-Apple")
+    }
+    
+//    private let appleLoginBtn = ASAuthorizationAppleIDButton(authorizationButtonType: .continue, authorizationButtonStyle: .white).then {
+//        $0.cornerRadius = 10
+//        $0.addTarget(self, action: #selector(tapAppleLoginBtn), for: .touchUpInside)
+//    }
 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .white
+        self.view.backgroundColor = .black
         
         setLayout()
         
+        let googleTapGesture = UITapGestureRecognizer(target: self, action: #selector(tapGoogleLoginBtn(_:)))
+        googleLoginView.addGestureRecognizer(googleTapGesture)
+        
+        let appleTapGesture = UITapGestureRecognizer(target: self, action: #selector(tapAppleLoginBtn(_:)))
+        appleLoginView.addGestureRecognizer(appleTapGesture)
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -56,21 +113,54 @@ class LoginViewController: UIViewController {
     }
     
     func setLayout() {
-        [googleLoginBtn, appleLoginBtn].forEach({ view.addSubview($0) })
-             
-        self.googleLoginBtn.snp.makeConstraints({
-            $0.top.equalToSuperview().offset(500)
+        [RutyIcon, RutyLogo, googleLoginView, appleLoginView].forEach({ view.addSubview($0) })
+        [googleStackView, googleLoginLabel, googleLoginLabelLogo].forEach({ googleLoginView.addSubview($0) })
+        [appleStackView, appleLoginLabel, appleLoginLabelLogo].forEach({ appleLoginView.addSubview($0) })
+        [googleLoginLabelLogo, googleLoginLabel].forEach({ googleStackView.addArrangedSubview($0) })
+        [appleLoginLabelLogo, appleLoginLabel].forEach({ appleStackView.addArrangedSubview($0) })
+
+        self.RutyIcon.snp.makeConstraints({
+            $0.top.equalToSuperview().inset(207)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(150)
+        })
+        
+        self.RutyLogo.snp.makeConstraints({
+            $0.top.equalTo(RutyIcon.snp.bottom).inset(7)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(34.47)
+            $0.width.equalTo(88.23)
+        })
+        
+        self.googleLoginView.snp.makeConstraints({
+            $0.bottom.equalTo(appleLoginView.snp.top).offset(-20)
             $0.leading.trailing.equalToSuperview().inset(20)
             $0.height.equalTo(56)
         })
-        self.appleLoginBtn.snp.makeConstraints({
-            $0.top.equalTo(googleLoginBtn.snp.bottom).offset(20)
+
+        self.googleStackView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        
+        self.googleLoginLabelLogo.snp.makeConstraints({
+            $0.height.width.equalTo(20)
+        })
+        
+        self.appleLoginView.snp.makeConstraints({
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-20)
             $0.leading.trailing.equalToSuperview().inset(20)
             $0.height.equalTo(56)
+        })
+
+        self.appleStackView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        
+        self.appleLoginLabelLogo.snp.makeConstraints({
+            $0.height.width.equalTo(20)
         })
     }
-    
-   
+
     @objc func tapGoogleLoginBtn(_ sender: UIButton) {
         // SDK 로그인 방식
         GIDSignIn.sharedInstance.signIn(withPresenting: self) { signInResult, error in
@@ -92,18 +182,19 @@ class LoginViewController: UIViewController {
                     do {
                         let decodedResponse = try JSONDecoder().decode(JSONModel.GoogleLoginResponse.self, from: data)
                         if decodedResponse.message == "ok" {
-                            print(decodedResponse)
+                            UserDefaults.standard.set(decodedResponse.data.accessToken, forKey: "accessToken")
+                            DataManager.shared.socialType = "GOOGLE"
                             // 로그인 성공
                             // 회원가입 or 메인뷰로 이동
                             self.loadMemberAgree()
                         }
                         else {
                             print("서버 연결 오류")
-                            //throw HttpError.badRequest
                             ErrorViewController.showErrorPage(viewController: self)
                         }
                     } catch {
                         print("JSON 디코딩 오류: \(error)")
+                        ErrorViewController.showErrorPage(viewController: self)
                     }
                 case .failure(let error):
                     // 요청이 실패한 경우
@@ -114,7 +205,7 @@ class LoginViewController: UIViewController {
         }
     }
     
-    @objc func tapAppleLoginBtn() {
+    @objc func tapAppleLoginBtn(_ sender: UIButton) {
         // SDK 로그인 방식
         let provider = ASAuthorizationAppleIDProvider()
         let requset = provider.createRequest()
@@ -142,15 +233,16 @@ class LoginViewController: UIViewController {
                     
                     // 회원가입 완료됨
                     else {
-                        self.moveToSignUp() // 디버깅용
-                        //self.moveToMainView() // 메인화면으로 이동
+                        //self.moveToSignUp() // 디버깅용
+                        self.moveToMainView() // 메인화면으로 이동
                     }
                 } catch {
                     print("JSON 디코딩 오류: \(error)")
+                    ErrorViewController.showErrorPage(viewController: self)
                 }
             case .failure(let error):
                 // 요청이 실패한 경우
-                print("API 요청 실패: \(error.localizedDescription)")
+                print("회원가입 API 요청 실패: \(error.localizedDescription)")
                 ErrorViewController.showErrorPage(viewController: self)
             }
         }
@@ -187,8 +279,6 @@ extension LoginViewController: ASAuthorizationControllerPresentationContextProvi
 extension LoginViewController: ASAuthorizationControllerDelegate {
     // 로그인 실패 시
     func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: any Error) {
-        print("로그인 실패", error.localizedDescription)
-        ErrorViewController.showErrorPage(viewController: self)
     }
     
     // Apple ID 로그인에 성공한 경우, 사용자의 인증 정보를 확인하고 필요한 작업을 수행합니다
@@ -203,16 +293,15 @@ extension LoginViewController: ASAuthorizationControllerDelegate {
                       var param = try? JSONSerialization.jsonObject(with: jsonData) as? [String: Any] else { return }
                 
                 UserDefaults.standard.set(authorizationCodeString, forKey: "authCode")
-                
-                print("애플 로그인 시도")
                 NetworkManager.shared.requestAPI(url: url, method: .post, encoding: JSONEncoding.default, param: param) { result in
                     switch result {
                     case .success(let data):
                         do {
                             let decodedResponse = try JSONDecoder().decode(JSONModel.AppleLoginResponse.self, from: data)
                             if decodedResponse.message == "ok" {
-                                print(decodedResponse)
-                                print("애플 로그인 성공")
+                                UserDefaults.standard.set(decodedResponse.data.accessToken, forKey: "accessToken")
+                                print("Apple 로그인 성공")
+                                DataManager.shared.socialType = "APPLE"
                                 // 로그인 성공. 회원가입 or 메인뷰로 이동
                                 self.loadMemberAgree()
                             }
@@ -222,6 +311,7 @@ extension LoginViewController: ASAuthorizationControllerDelegate {
                             }
                         } catch {
                             print("JSON 디코딩 오류: \(error)")
+                            ErrorViewController.showErrorPage(viewController: self)
                         }
                     case .failure(let error):
                         // 요청이 실패한 경우
